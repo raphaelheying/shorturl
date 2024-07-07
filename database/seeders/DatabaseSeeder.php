308a2397784728c20ce4a859b07e3e2026db88db
Raphael Heying <raphael.h@hotmail.com>
2024-07-07 20:01:48 -0300

    seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\{Link, User, Visit};
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -10,10 +10,24 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
-            'name'     => 'Raphael Heying',
-            'email'    => 'raphael.h@hotmail.com',
-            'password' => Hash::make('password'),
-        ]);
+        if (app()->environment('local')) {
+            $user = User::factory()->create([
+                'name'     => 'Raphael Heying',
+                'email'    => 'raphael.h@hotmail.com',
+                'password' => Hash::make('password'),
+            ]);
+
+            $links = Link::factory()->count(rand(30, 50))->create([
+                'user_id' => $user->id,
+            ]);
+
+            $links->each(function ($link) {
+                Visit::factory()->count(rand(0, 100))->create([
+                    'link_id' => $link->id,
+                ]);
+            });
+
+        }
+
     }
 }
